586af6a6907508802f4d292dea93f33db9dcc4b1
Adil Shabab <adilshaba@gmail.com>
2022-06-21 08:29:39 -0700

    admin added in core

--- behance/core/admin.py
+++ behance/core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Work, Tags, Tools_used, Comment
 # Register your models here.
+
+admin.site.register(Work)
+admin.site.register(Tags)
+admin.site.register(Tools_used)
+admin.site.register(Comment)
