0da1d66c755c33319811fce3a7a26acc9ac37eb7
Adil Shabab <adilshaba@gmail.com>
2022-06-21 08:36:06 -0700

    signals added in accounts

---
+++ behance/accounts/signals.py
@@ -0,0 +1,38 @@
+from django.db.models.signals import post_save, post_delete
+
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from .models import Profile
+
+
+def createProfile(sender, instance, created, **kwargs):
+    print("profile signal trigger")
+    if created:
+        user = instance
+        profile = Profile.objects.create(
+            user=user,
+            username=user.username,
+            email=user.email,
+            name=user.first_name,
+        )
+
+       
+
+def deleteUser(sender, instance, **kwargs):
+    user = instance.user
+    user.delete()
+
+def updateUser(sender, instance, created, **kwargs):
+    profile = instance
+    user = profile.user
+    if created == False:
+        user.first_name = profile.name
+        user.username = profile.username
+        user.email = profile.email
+        user.save()
+
+
+post_save.connect(createProfile, sender=User)
+post_delete.connect(deleteUser, sender= Profile)
+
+post_save.connect(updateUser, sender=Profile)
